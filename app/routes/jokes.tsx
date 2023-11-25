dbdc7f9aa2bd15c7f38be6e28f372964016c38e2
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-11-25 04:50:41 +0000

    ✨  User auth feature

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,18 +1,28 @@
-import { json, type LinksFunction } from "@remix-run/node";
+import {
+  json,
+  type LinksFunction,
+  type LoaderFunctionArgs,
+} from "@remix-run/node";
 import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
 import stylesUrl from "~/styles/jokes.css";
 import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
 ];
 
-export const loader = async () =>{
-  return json({
-    jokeListItems: await db.joke.findMany(),
-  })
-}
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const jokeListItems = await db.joke.findMany({
+    orderBy: { createdAt: "desc" },
+    select: { id: true, name: true },
+    take: 5,
+  });
+  const user = await getUser(request);
+
+  return json({ jokeListItems, user });
+};
 
 export default function JokesRoute() {
   const data = useLoaderData<typeof loader>();
@@ -21,15 +31,23 @@ export default function JokesRoute() {
       <header className="jokes-header">
         <div className="container">
           <h1 className="home-link">
-            <Link
-              to="/"
-              title="Remix Jokes"
-              aria-label="Remix Jokes"
-            >
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
               <span className="logo">🤪</span>
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
@@ -38,9 +56,9 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              {data.jokeListItems.map(({id, name})=>(
+              {data.jokeListItems.map(({ id, name }) => (
                 <li key={id}>
-                  <Link to={id}>{name}</Link>
+                  <Link prefetch="intent" to={id}>{name}</Link>
                 </li>
               ))}
             </ul>
